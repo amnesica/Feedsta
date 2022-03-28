6f3d69692ca8ad335e33796e3a9f99ae8f32f3e4
amnesica <54629966+amnesica@users.noreply.github.com>
2022-03-28 17:26:18 +0200

    Increase fetch values for feed

--- app/src/main/java/com/amnesica/feedsta/helper/FeedHelper.java
+++ app/src/main/java/com/amnesica/feedsta/helper/FeedHelper.java
@@ -6,13 +6,13 @@ package com.amnesica.feedsta.helper;
 public class FeedHelper {
     // amount of posts to be fetched from a single account in feed
     // hint: 12 is the common size of a page
-    public static final int counterPostBorder = 12;
+    public static final int counterPostBorder = 48;
 
     // amount of posts to be fetched on a single page
     // hint: 12 is the common size of a page
-    public static final int fetchBorderPerPage = 12;
+    public static final int fetchBorderPerPage = 48;
 
     // overall amount of posts in feed
     // hint: to prevent OutOfMemoryException this border might be lower
-    public static final int counterFeedBorder = 300;
+    public static final int counterFeedBorder = 400;
 }
