4557bded72f0909d3c4775d01b84ccae9c65321c
amnesica <54629966+amnesica@users.noreply.github.com>
2022-08-30 10:41:45 +0200

    Remove unused interface

--- app/src/main/java/com/amnesica/feedsta/interfaces/DownloadMediaCallback.java
+++
@@ -1,7 +0,0 @@
-package com.amnesica.feedsta.interfaces;
-
-public interface DownloadMediaCallback {
-  void showImageDownloadResult(boolean downloadSuccessful);
-
-  void showVideoDownloadResult(boolean downloadSuccessful);
-}
